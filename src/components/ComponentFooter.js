0bd8f6e1b5c8f344a59297a42a47d8189dae6725
sachinfromtokopedia <sachin.sharma@tokopedia.com>
2021-12-11 12:04:21 +0530

    added tagging feature

--- src/components/ComponentFooter.js
+++ src/components/ComponentFooter.js
@@ -2,10 +2,10 @@ import { Button, Layout, Row } from 'antd';
 const { Footer} = Layout;
 
 function ComponentFooter(){
-    return <Footer style={{ textAlign: 'center' }}>
+    return <Footer style={{ display:'flex' , justifyContent:'space-between' }}>
             
-            <Button>Add more products</Button>            
-            <Button>Export</Button>            
+            <Button type="primary">Add more products</Button>            
+            <Button type="primary">Export</Button>            
     </Footer>
  
 }
--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,14 +1,53 @@
 
-
+import { useState, useEffect } from 'react';
 import { Layout } from 'antd';
-const {  Content } = Layout;
-function Playground(){
+import RegionSelect from "react-region-select";
+import { Card } from 'antd';
+import { DeleteFilled } from '@ant-design/icons';
+
+const { Content } = Layout;
+function Playground() {
+
+    const [regions, setRegions] = useState([]);
+
+    function onChange(regions) {
+        setRegions(regions)
+    }
+
+   
+    function regionRenderer(regionProps) {
+        console.log("region propd" , regionProps);
+        if (!regionProps.isChanging) {
+            return (
+                <div style={{ position: "absolute", right: "50%", top :"50%"}} title={"Apple glasses"}>
+                    <DeleteFilled  style={{color : "red"}}onClick={()=>{setRegions(regions.filter(el=>el.data.index!==regionProps.data.index))}}/>
+                </div>
+            );
+        }
+    }
+
+    let regionStyle = {
+        background: "rgba(0, 255, 0, 0.5)",
+        zIndex: 99
+    }
+
     return <Content style={{ margin: '24px 16px 0' }}>
-    <div className="site-layout-background" style={{ padding: 24,height:"100%" , border :"1px solid" }}>
-      content
-    </div>
-    
-  </Content>
+        <Card title="Playground" style={{ padding: 5, height: "100%" }}>
+            <RegionSelect
+                maxRegions={10}
+                regions={regions}
+                regionStyle={regionStyle}
+                onChange={onChange}
+                regionRenderer={regionRenderer}
+                style={{ height: "100%", width: "100%" }}
+                constraint={true}
+            >
+                <img src={"/dummy.jpeg"} style={{ width: "100%", height: "100%" }} />
+            </RegionSelect>
+
+        </Card>
+
+    </Content>
 
 }
 
